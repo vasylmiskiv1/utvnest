f044854ad787a25f64220e007602df625a9558b4
Vasyl <vasiliy.miskiv@elnk.com>
2022-12-13 01:15:57 +0200

    created roles sheets, dto, controller and services

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger/dist/decorators";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { User } from "src/users/users.model";
+import { UserRoles } from "./user-roles.model";
+
+interface RoleCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: `roles` })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: "1", description: "Unique id" })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({ example: "Admin", description: "User's role" })
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  value: string;
+
+  @ApiProperty({ example: "Administrator", description: "Role description" })
+  @Column({ type: DataType.STRING, allowNull: false })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger/dist/decorators";
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { Role } from "src/roles/roles.model";
+import { UserRoles } from "src/roles/user-roles.model";
 
 interface UserCreationAttrs {
   email: string;
@@ -28,4 +30,7 @@ export class User extends Model<User, UserCreationAttrs> {
   @ApiProperty({example: 'reason', description: 'ban reason'})
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  roles: Role[];
 }
